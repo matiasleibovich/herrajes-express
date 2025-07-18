8ff6b2658705546d01853f60514531260196bf2e
matiasleibovich <matias.leibovich@gmail.com>
2025-07-18 19:01:00 -0300

    secciones de la home

---
+++ home_catalogo.php
@@ -0,0 +1,51 @@
+		<!-- Action box element style 1 (full-width) - section with custom paddings -->
+		<section class="hg_section_simon">
+			<div class="full-width">
+				<div class="row">
+					<div class="col-sm-12 col-md-12">
+
+
+						<!-- Action box element style 1 arrow center -->
+						<div class="action_box style1" data-arrowpos="center">
+							<div class="action_box_inner container">
+								<div class="action_box_content row">
+									<!-- Content -->
+									<div class="ac-content-text col-sm-12 col-md-12 col-lg-7 mb-md-20">
+										<!-- Title -->
+										<h4 class="text">
+											Conoce todos nuestros productos!
+										</h4>
+
+										<!-- Sub-title -->
+										<h5 class="ac-subtitle">
+											Tenemos todos los accesorios para Carpinter&iacute;a de Aluminio.<br>
+											Materiales nacionales e importados.
+										</h5>
+									</div>
+									<!--/ Content col-sm-12 col-md-12 col-lg-7 mb-md-20 -->
+
+									<!-- Call to Action buttons -->
+									<div class="ac-buttons col-sm-12 col-md-12 col-lg-5 d-flex align-self-center justify-content-center justify-content-lg-end">
+										<a href="Catalogo-Herrajes-Express.pdf" target="catalogo_online" class="btn btn-lined ac-btn" title="HERRAJES EXPRESS">
+											VER CATALOGO ONLINE &darr; 25MB
+										</a>
+
+										<a href="/contacto" target="_blank" class="btn btn-fullwhite ac-btn" title="Contactar HERRAJES EXPRESS">
+											CONTACTANOS
+										</a>
+									</div>
+									<!--/ Call to Action buttons -->
+								</div>
+								<!--/ .action_box_content -->
+							</div>
+							<!--/ .action_box_inner -->
+						</div>
+						<!--/ Action box element style 1 .action_box -->
+					</div>
+					<!--/ col-md-12 col-sm-12 -->
+				</div>
+				<!--/ row -->
+			</div>
+			<!--/ .full-width -->
+		</section>
+		<!--/ Action box element style 1 (full-width) - section with custom paddings -->
---
+++ home_productos.php
@@ -0,0 +1,474 @@
+		<!-- Products category section with custom top padding -->
+		<section class="hg_section pt-80 pb-80">
+			<div class="container">
+				<div class="row">
+					<!-- Content with left sidebar -->
+					<div class="col-sm-12 col-md-12">
+						<!-- Title with bold style -->
+						<h2 class="page-title subtitle_simon fw-book">
+							ACCESORIOS PARA CARPINTERIA DE ALUMINIO:
+						</h2>
+
+						<!-- Products list -->
+						<ul class="products clearfix">
+
+
+							<!-- Product #1 -->
+							<li class="product">
+								<div class="product-list-item prod-layout-classic">
+
+									<!-- Badge container -->
+									<!--simon <div class="hg_badge_container">
+										<span class="hg_badge_sale">
+											ALTA GAMA
+										</span>
+										<span class="hg_badge_new">
+											NUEVA!
+										</span>
+									</div> simon-->
+									<!--/ Badge container -->
+
+									<!-- Product container link -->
+									<a href="bisagras.html">
+										<!-- Image wrapper -->
+										<span class="image kw-prodimage">
+											<!-- Primary image -->
+											<img class="kw-prodimage-img" src="images/325x325/002.webp" alt="HERRAJES EXPRESS" title="HERRAJES EXPRESS" />
+										</span>
+										<!--/ Image wrapper -->
+
+										<!-- Details -->
+										<div class="details kw-details_simon fixclear">
+											<!-- Title -->
+											<h3 class="kw-details-title">
+												BISAGRAS<br>
+											</h3>
+										</div>
+										<!--/ details fixclear -->
+									</a>
+									<!-- Product container link -->
+
+								</div>
+								<!--/ product-list-item -->
+							</li>
+							<!--/ Product #1 -->
+
+							<!-- Product #2 -->
+							<li class="product">
+								<div class="product-list-item prod-layout-classic">
+
+									<!-- Badge container -->
+									<!--simon <div class="hg_badge_container">
+										<span class="hg_badge_sale">
+											ALTA GAMA
+										</span>
+										<span class="hg_badge_new">
+											NUEVA!
+										</span>
+									</div> simon-->
+									<!--/ Badge container -->
+
+									<!-- Product container link -->
+									<a href="bocallaves.html">
+										<!-- Image wrapper -->
+										<span class="image kw-prodimage">
+											<!-- Primary image -->
+											<img class="kw-prodimage-img" src="images/325x325/009.webp" alt="HERRAJES EXPRESS" title="HERRAJES EXPRESS" />
+										</span>
+										<!--/ Image wrapper -->
+
+										<!-- Details -->
+										<div class="details kw-details_simon fixclear">
+											<!-- Title -->
+											<h3 class="kw-details-title">
+												BOCALLAVES<br>
+											</h3>
+										</div>
+										<!--/ details fixclear -->
+									</a>
+									<!-- Product container link -->
+
+								</div>
+								<!--/ product-list-item -->
+							</li>
+							<!--/ Product #2 -->
+
+							<!-- Product #3 -->
+							<li class="product">
+								<div class="product-list-item prod-layout-classic">
+
+									<!-- Badge container -->
+									<!--simon <div class="hg_badge_container">
+										<span class="hg_badge_sale">
+											ALTA GAMA
+										</span>
+										<span class="hg_badge_new">
+											NUEVA!
+										</span>
+									</div> simon-->
+									<!--/ Badge container -->
+
+									<!-- Product container link -->
+									<a href="cerraduras.html">
+										<!-- Image wrapper -->
+										<span class="image kw-prodimage">
+											<!-- Primary image -->
+											<img class="kw-prodimage-img" src="images/325x325/004.webp" alt="HERRAJES EXPRESS" title="HERRAJES EXPRESS" />
+										</span>
+										<!--/ Image wrapper -->
+
+										<!-- Details -->
+										<div class="details kw-details_simon fixclear">
+											<!-- Title -->
+											<h3 class="kw-details-title">
+												CERRADURAS<br>
+											</h3>
+										</div>
+										<!--/ details fixclear -->
+									</a>
+									<!-- Product container link -->
+
+								</div>
+								<!--/ product-list-item -->
+							</li>
+							<!--/ Product #3 -->
+
+							<!-- Product #4 -->
+							<li class="product">
+								<div class="product-list-item prod-layout-classic">
+
+									<!-- Badge container -->
+									<!--simon <div class="hg_badge_container">
+										<span class="hg_badge_sale">
+											ALTA GAMA
+										</span>
+										<span class="hg_badge_new">
+											NUEVA!
+										</span>
+									</div> simon-->
+									<!--/ Badge container -->
+
+									<!-- Product container link -->
+									<a href="cierres.html">
+										<!-- Image wrapper -->
+										<span class="image kw-prodimage">
+											<!-- Primary image -->
+											<img class="kw-prodimage-img" src="images/325x325/008.webp" alt="HERRAJES EXPRESS" title="HERRAJES EXPRESS" />
+										</span>
+										<!--/ Image wrapper -->
+
+										<!-- Details -->
+										<div class="details kw-details_simon fixclear">
+											<!-- Title -->
+											<h3 class="kw-details-title">
+												CIERRES<br>
+											</h3>
+										</div>
+										<!--/ details fixclear -->
+									</a>
+									<!-- Product container link -->
+
+								</div>
+								<!--/ product-list-item -->
+							</li>
+							<!--/ Product #4 -->
+
+							<!-- Product #5 -->
+							<li class="product">
+								<div class="product-list-item prod-layout-classic">
+
+									<!-- Badge container -->
+									<!--simon <div class="hg_badge_container">
+										<span class="hg_badge_sale">
+											ALTA GAMA
+										</span>
+										<span class="hg_badge_new">
+											NUEVA!
+										</span>
+									</div> simon-->
+									<!--/ Badge container -->
+
+									<!-- Product container link -->
+									<a href="escuadras.html">
+										<!-- Image wrapper -->
+										<span class="image kw-prodimage">
+											<!-- Primary image -->
+											<img class="kw-prodimage-img" src="images/325x325/001.webp" alt="HERRAJES EXPRESS" title="HERRAJES EXPRESS" />
+										</span>
+										<!--/ Image wrapper -->
+
+										<!-- Details -->
+										<div class="details kw-details_simon fixclear">
+											<!-- Title -->
+											<h3 class="kw-details-title">
+												ESCUADRAS<br>
+											</h3>
+										</div>
+										<!--/ details fixclear -->
+									</a>
+									<!-- Product container link -->
+
+								</div>
+								<!--/ product-list-item -->
+							</li>
+							<!--/ Product #5 -->
+
+							<!-- Product #6 -->
+							<li class="product">
+								<div class="product-list-item prod-layout-classic">
+
+									<!-- Badge container -->
+									<!--simon <div class="hg_badge_container">
+										<span class="hg_badge_sale">
+											ALTA GAMA
+										</span>
+										<span class="hg_badge_new">
+											NUEVA!
+										</span>
+									</div> simon-->
+									<!--/ Badge container -->
+
+									<!-- Product container link -->
+									<a href="felpas.html">
+										<!-- Image wrapper -->
+										<span class="image kw-prodimage">
+											<!-- Primary image -->
+											<img class="kw-prodimage-img" src="images/325x325/015.webp" alt="HERRAJES EXPRESS" title="HERRAJES EXPRESS" />
+										</span>
+										<!--/ Image wrapper -->
+
+										<!-- Details -->
+										<div class="details kw-details_simon fixclear">
+											<!-- Title -->
+											<h3 class="kw-details-title">
+												FELPAS<br>
+											</h3>
+										</div>
+										<!--/ details fixclear -->
+									</a>
+									<!-- Product container link -->
+
+								</div>
+								<!--/ product-list-item -->
+							</li>
+							<!--/ Product #6 -->
+
+
+
+
+							<!-- Product #8 -->
+							<li class="product">
+								<div class="product-list-item prod-layout-classic">
+
+									<!-- Badge container -->
+									<!--simon <div class="hg_badge_container">
+										<span class="hg_badge_sale">
+											ALTA GAMA
+										</span>
+										<span class="hg_badge_new">
+											NUEVA!
+										</span>
+									</div> simon-->
+									<!--/ Badge container -->
+
+									<!-- Product container link -->
+									<a href="manijas.html">
+										<!-- Image wrapper -->
+										<span class="image kw-prodimage">
+											<!-- Primary image -->
+											<img class="kw-prodimage-img" src="images/325x325/010.webp" alt="HERRAJES EXPRESS" title="HERRAJES EXPRESS" />
+										</span>
+										<!--/ Image wrapper -->
+
+										<!-- Details -->
+										<div class="details kw-details_simon fixclear">
+											<!-- Title -->
+											<h3 class="kw-details-title">
+												MANIJAS<br>
+											</h3>
+										</div>
+										<!--/ details fixclear -->
+									</a>
+									<!-- Product container link -->
+
+								</div>
+								<!--/ product-list-item -->
+							</li>
+							<!--/ Product #8 -->
+
+							<!-- Product #9 -->
+							<li class="product">
+								<div class="product-list-item prod-layout-classic">
+
+									<!-- Badge container -->
+									<!--simon <div class="hg_badge_container">
+										<span class="hg_badge_sale">
+											ALTA GAMA
+										</span>
+										<span class="hg_badge_new">
+											NUEVA!
+										</span>
+									</div> simon-->
+									<!--/ Badge container -->
+
+									<!-- Product container link -->
+									<a href="oscilo-batientes.html">
+										<!-- Image wrapper -->
+										<span class="image kw-prodimage">
+											<!-- Primary image -->
+											<img class="kw-prodimage-img" src="images/325x325/016.webp" alt="HERRAJES EXPRESS" title="HERRAJES EXPRESS" />
+										</span>
+										<!--/ Image wrapper -->
+
+										<!-- Details -->
+										<div class="details kw-details_simon fixclear">
+											<!-- Title -->
+											<h3 class="kw-details-title">
+												OSCILO BATIENTES<br>
+											</h3>
+										</div>
+										<!--/ details fixclear -->
+									</a>
+									<!-- Product container link -->
+
+								</div>
+								<!--/ product-list-item -->
+							</li>
+							<!--/ Product #9 -->
+
+							<!-- Product #10 -->
+							<li class="product">
+								<div class="product-list-item prod-layout-classic">
+
+									<!-- Badge container -->
+									<!--simon <div class="hg_badge_container">
+										<span class="hg_badge_sale">
+											ALTA GAMA
+										</span>
+										<span class="hg_badge_new">
+											NUEVA!
+										</span>
+									</div> simon-->
+									<!--/ Badge container -->
+
+									<!-- Product container link -->
+									<a href="plasticos.html">
+										<!-- Image wrapper -->
+										<span class="image kw-prodimage">
+											<!-- Primary image -->
+											<img class="kw-prodimage-img" src="images/325x325/014.webp" alt="HERRAJES EXPRESS" title="HERRAJES EXPRESS" />
+										</span>
+										<!--/ Image wrapper -->
+
+										<!-- Details -->
+										<div class="details kw-details_simon fixclear">
+											<!-- Title -->
+											<h3 class="kw-details-title">
+												PLASTICOS<br>
+											</h3>
+										</div>
+										<!--/ details fixclear -->
+									</a>
+									<!-- Product container link -->
+
+								</div>
+								<!--/ product-list-item -->
+							</li>
+							<!--/ Product #10 -->
+
+							<!-- Product #11 -->
+							<li class="product">
+								<div class="product-list-item prod-layout-classic">
+
+									<!-- Badge container -->
+									<!--simon <div class="hg_badge_container">
+										<span class="hg_badge_sale">
+											ALTA GAMA
+										</span>
+										<span class="hg_badge_new">
+											NUEVA!
+										</span>
+									</div> simon-->
+									<!--/ Badge container -->
+
+									<!-- Product container link -->
+									<a href="ruedas.html">
+										<!-- Image wrapper -->
+										<span class="image kw-prodimage">
+											<!-- Primary image -->
+											<img class="kw-prodimage-img" src="images/325x325/003.webp" alt="HERRAJES EXPRESS" title="HERRAJES EXPRESS" />
+										</span>
+										<!--/ Image wrapper -->
+
+										<!-- Details -->
+										<div class="details kw-details_simon fixclear">
+											<!-- Title -->
+											<h3 class="kw-details-title">
+												RUEDAS<br>
+											</h3>
+										</div>
+										<!--/ details fixclear -->
+									</a>
+									<!-- Product container link -->
+
+								</div>
+								<!--/ product-list-item -->
+							</li>
+							<!--/ Product #11 -->
+
+							<!-- Product #12 -->
+							<li class="product">
+								<div class="product-list-item prod-layout-classic">
+
+									<!-- Badge container -->
+									<!--simon <div class="hg_badge_container">
+										<span class="hg_badge_sale">
+											ALTA GAMA
+										</span>
+										<span class="hg_badge_new">
+											NUEVA!
+										</span>
+									</div> simon-->
+									<!--/ Badge container -->
+
+									<!-- Product container link -->
+									<a href="varios.html">
+										<!-- Image wrapper -->
+										<span class="image kw-prodimage">
+											<!-- Primary image -->
+											<img class="kw-prodimage-img" src="images/_shop/varios/325x325/h53_brazo_limitador.webp" alt="HERRAJES EXPRESS" title="HERRAJES EXPRESS" />
+										</span>
+										<!--/ Image wrapper -->
+
+										<!-- Details -->
+										<div class="details kw-details_simon fixclear">
+											<!-- Title -->
+											<h3 class="kw-details-title">
+												VARIOS<br>
+											</h3>
+										</div>
+										<!--/ details fixclear -->
+									</a>
+									<!-- Product container link -->
+
+								</div>
+								<!--/ product-list-item -->
+							</li>
+							<!--/ Product #12 -->
+
+
+
+						</ul>
+						<!--/ Products list -->
+					</div>
+					<!--/ Content with left sidebar col-sm-12 col-md-12 col-lg-9 order-lg-1 -->
+
+
+
+
+				</div>
+				<!--/ row -->
+			</div>
+			<!--/ container -->
+		</section>
+		<!--/ Products category section with custom top padding -->
